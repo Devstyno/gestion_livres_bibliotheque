f9500ec8b215aa1fcf7821df47c97242d16fa57f
devstyno <ddevstyno@gmail.com>
2024-07-01 19:08:23 +0000

    parametrage de l'interface d'administration

--- api/bibliotheque/admin.py
+++ api/bibliotheque/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from bibliotheque.models import Categorie, Livre
 
 # Register your models here.
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'libelle', 'date_created', 'date_updated', 'active')
+
+class LivreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'isbn', 'titre', 'categorie', 'auteur', 'editeur', 'date_created', 'date_updated', 'active')
+
+admin.site.register(Categorie, CategorieAdmin)
+admin.site.register(Livre, LivreAdmin)
