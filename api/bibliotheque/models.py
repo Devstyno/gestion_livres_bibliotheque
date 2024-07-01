dc85de590d4669b312eb97f6fe53a88805a82302
devstyno <ddevstyno@gmail.com>
2024-07-01 19:07:02 +0000

    definition des modeles

--- api/bibliotheque/models.py
+++ api/bibliotheque/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Categorie(models.Model):
+    libelle = models.CharField(max_length=255, blank=False, null=False)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.libelle
+
+class Livre(models.Model):
+    isbn = models.CharField(max_length=255, unique=True)
+    titre = models.CharField(max_length=255)
+    auteur = models.CharField(max_length=255)
+    editeur = models.CharField(max_length=255)
+    categorie = models.ForeignKey(Categorie, on_delete=models.DO_NOTHING, related_name="livres")
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.titre
